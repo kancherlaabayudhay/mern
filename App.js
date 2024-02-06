82de59fcbc6ecc7793994bf779358415617b8a6b
kancherlaabayudhay <158036810+kancherlaabayudhay@users.noreply.github.com>
2024-02-06 16:00:54 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import welcome from './component/welcome';
+import student from './component/student';
+
+function App() {
+  return (
+    <div>
+      <Welcome></Welcome>
+      <Student></Student>    
+    </div>
+  );
+}
+
+export default App;
---
+++ student.js
@@ -0,0 +1,16 @@
+import React from "react";
+const Student=()=>
+{
+    return(
+        <div>
+            <table border = "3">
+            <tr><th>Student name</th><th>Student number</th></tr>
+                <tr><td>Abyudhay</td><td>1</td></tr>
+                <tr><td>HRUDHAY</td><td>2</td></tr>
+             
+            </table>
+            
+        </div>
+    );
+};
+export default Student;
